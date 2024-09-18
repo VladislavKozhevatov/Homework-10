dd59346406cf5590b88896635a739e7e1ff999f4
Vladislav <tekuzar@mail,ru>
2024-09-18 20:28:41 +0300

    Done!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-    //Задача 1
+        //Задача 1
         System.out.println("Задача 1");
         String firstName = "Ivan ";
         String middleName = "Ivanov ";
@@ -8,11 +8,11 @@ public class Main {
         String fullName = middleName+firstName+lastName;
         System.out.println("Ф. И. О. сотрудника — "+fullName);
         System.out.println();
-    //Задача 2
+        //Задача 2
         System.out.println("Задача 2");
         System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета —"+fullName.toUpperCase());
         System.out.println();
-    //Задача 3
+        //Задача 3
         System.out.println("Задача 3");
         String firstName1 = "Семён ";
         String middleName1 = "Иванов ";
